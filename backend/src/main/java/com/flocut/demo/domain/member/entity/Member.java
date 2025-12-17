054b2fed2887f8f350f4cac6128b3989f0611d47
bschil00-png <bschil00@gmail.com>
2025-12-17 13:23:53 +0900

    테이블명 수정

--- backend/src/main/java/com/flocut/demo/domain/member/entity/Member.java
+++ backend/src/main/java/com/flocut/demo/domain/member/entity/Member.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.ColumnDefault;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "tbl_member_test")
+@Table(name = "tbl_member")
 @Getter
 @Setter
 @NoArgsConstructor
