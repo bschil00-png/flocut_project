0beafae5d661c3e24493861c240eeff9fc48958e
bschil00-png <bschil00@gmail.com>
2025-12-26 12:40:20 +0900

    memberdto 수정

--- backend/src/main/java/com/flocut/demo/domain/member/dto/MemberDto.java
+++ backend/src/main/java/com/flocut/demo/domain/member/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package com.flocut.demo.domain.member.dto;
 
+import com.flocut.demo.domain.member.entity.UserRole;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,4 +17,5 @@ public class MemberDto {
     private Boolean emailVerified;
     private String regdate;
     private String moddate;
+    private UserRole role;
 }
