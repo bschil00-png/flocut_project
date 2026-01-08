a439d22447f128771e9aa8b505145081f36d352b
sr0116 <kysy0116@gmail.com>
2026-01-08 17:08:26 +0900

    임시 커밋

--- frontend/src/app/(workspace)/settings/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ProfilePanel from "@/app/components/settings/panels/ProfilePanel";
-
-export default function Page() {
-    return <ProfilePanel />;
-}
--- frontend/src/app/components/ui/button/index.ts
+++
@@ -1,3 +0,0 @@
-// ui/button/index.ts
-export { default as Button } from "./Button";
-export type { ButtonProps } from "./Button";
