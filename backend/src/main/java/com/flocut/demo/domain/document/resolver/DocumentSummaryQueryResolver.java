4c66ecfa382dda094e42acdae6579498781a77c4
sr0116 <kysy0116@gmail.com>
2026-01-16 12:50:44 +0900

    서버 배포용

--- backend/src/main/java/com/flocut/demo/domain/document/resolver/DocumentSummaryQueryResolver.java
+++ backend/src/main/java/com/flocut/demo/domain/document/resolver/DocumentSummaryQueryResolver.java
@@ -10,8 +10,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
 public class DocumentSummaryQueryResolver {
