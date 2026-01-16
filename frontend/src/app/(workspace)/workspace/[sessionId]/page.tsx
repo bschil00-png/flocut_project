17c8f54ef5ffc9cd1f48862eb42229f5273c6866
sr0116 <kysy0116@gmail.com>
2026-01-16 11:55:22 +0900

    마지막 전체 보완

--- frontend/src/app/(workspace)/workspace/[sessionId]/page.tsx
+++
@@ -1,7 +0,0 @@
-import WorkspacePage from "@/app/components/layout/WorkspaceLayout/workspace/WorkspacePage";
-
-
-
-export default function SessionPage() {
-    return <WorkspacePage />;
-}
--- frontend/src/app/(workspace)/workspace/sessions/page.tsx
+++
@@ -1,7 +0,0 @@
-import SessionList from "@/app/components/sessions/SessionList";
-
-
-
-export default function SessionsPage() {
-    return <SessionList />;
-}
